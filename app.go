f3b081fadc9dc7dff38843b42e6205765d5f377e
Joseph Egboka <joeegboka@gmail.com>
2026-04-03 13:23:23 -0700

    calculate future interest value

--- app.go
+++ app.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello World!")
+	const inflationRate = 6.5
+	years := 10.0
+	var investmentAmount float64 = 1000
+	expectedReturnRate := 5.5
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	fmt.Println("Future Value of Investment:", futureValue)
 }
